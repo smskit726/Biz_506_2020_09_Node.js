cdcf1b1c0085bf0bd5c5d01e9c5980558c2b4ae9
smskit726 <smskit726@gmail.com>
2020-09-16 15:23:07 +0900

    moment 사용하기

--- Hello_Node_3/routes/index.js
+++ Hello_Node_3/routes/index.js
@@ -5,9 +5,11 @@ const moment = require("moment");
 /* GET home page. */
 router.get("/", function (req, res, next) {
   let date = moment(new Date()).format("YYYY-MM-DD");
+  let time = moment(new Date()).format("HH:mm:ss");
   res.render("index", {
     title: "반갑습니다",
     date: date,
+    time: time,
   });
 });
 
